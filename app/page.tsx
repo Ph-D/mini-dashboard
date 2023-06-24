4c7026a72277f5bd588a9b459f1ea68725cb7d13
Philippe Doreau <p.doreau@criteo.com>
2023-06-25 01:24:11 +0200

    [MiniDashboard] - loader implementation

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
       <SidePanel />
-      <div className="flex-1 overflow-auto p-5 lg:p-10">
         <MainPanel />
-      </div>
     </div>
   );
 }
--- components/mainpanel/index.tsx
+++ components/mainpanel/index.tsx
@@ -4,6 +4,7 @@ import { useCryptoStore } from "@/store/CryptoStore";
 import CryptoChart from "../charts/CryptoChart";
 import InformationCard from "./InformationCard";
 import GridCard from "./GridCard";
+import Loader from "../layouts/Loader";
 
 function MainPanel() {
   const { fetchDataState, fetchLoadingState } = useCryptoStore();
@@ -11,7 +12,7 @@ function MainPanel() {
   return (
     <>
       {fetchLoadingState === "success" ? (
-        <>
+        <div className="flex-1 overflow-auto p-5 lg:p-10 relative">
           <div className="mb-5">
             <InformationCard results={fetchDataState} />
           </div>
@@ -19,9 +20,11 @@ function MainPanel() {
             <GridCard results={fetchDataState} />
           </div>
           <CryptoChart results={fetchDataState} />
-        </>
+        </div>
       ) : (
-        <div>test</div>
+        <div className="flex-1 relative">
+          <Loader />
+        </div>
       )}
     </>
   );
